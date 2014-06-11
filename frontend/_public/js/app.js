8acd35f88bf4c00b5d10dbfae3bb408af477749a
Maximilian Stroh <Hisako1337@gmail.com>
2014-06-11 15:20:32 +0200

    readme compiled

--- frontend/_public/js/app.js
+++ frontend/_public/js/app.js
@@ -1,1 +1,165 @@
-"use strict";var App;App=angular.module("app",["ngCookies","ngResource","ngRoute","app.controllers","app.directives","app.filters","app.services","partials"]),App.config(["$routeProvider","$locationProvider",function(n,e){return n.when("/",{templateUrl:"/partials/landingpage.html"}).when("/pictures",{templateUrl:"/partials/pictures.html"}).otherwise({redirectTo:"/"}),e.html5Mode(!1)}]),angular.module("app.controllers",[]).controller("AppCtrl",["$scope","$location","$resource","$rootScope",function(n,e){return n.$location=e,n.$watch("$location.path()",function(e){return n.activeNavId=e||"/"}),n.getClass=function(e){return n.activeNavId.substring(0,e.length)===e?"active":""}}]).controller("PicturesCtrl",["$scope","Pictures",function(n,e){return n.currentPictures=[],e.getInitialPics(function(e,t){return e?console.log(e):n.currentPictures=t.data}),n.requestPictures=function(t){return e.getForKeywords(t,function(e,t){return e?alert(e):(console.log(t.data),n.currentPictures=t.data)})},n.nextPicturesByUrl=function(e){var t;return t=_.filter(n.currentPictures,function(n){return n.url===e})[0],n.requestPictures((null!=t?t.tags:void 0)||[])},n.nextPicturesByTag=function(e){return n.requestPictures([e])}}]).controller("LoginCtrl",["$scope","User",function(n,e){return n.loginNewUser=function(){var t;return t=n.newUser.nodeId,e.login(function(n,e){return n?console.log("LoginError! ",n):console.log("Logged IN! ",e)})}}]),angular.module("app.directives",["app.services"]).directive("appVersion",["version",function(n){return function(e,t){return t.text(n)}}]),angular.module("app.filters",[]).filter("interpolate",["version",function(n){return function(e){return String(e).replace(/\%VERSION\%/gm,n)}}]),angular.module("app.services",[]).service("User",["$http",function(n){var e;return e={name:null,loggedIn:!1},{currentUser:function(){return e},login:function(e,t){return n.post("/login",{id:e}).then(function(n){return"function"==typeof t?t(null,n):void 0},function(n){return"function"==typeof t?t({error:n.error||"Node ID invalid!"},null):void 0})},logout:function(){return n.post("/logout").then(function(){return e={name:null,loggedIn:!1}},function(){return e={name:null,loggedIn:!1}})},checkSession:function(){return n.get("/loggedin").then(function(n){return e.name=n.data.name,e.loggedIn=!0},function(){})}}}]).service("Pictures",["$http",function(n){return{getForKeywords:function(e,t){return n.get("/pictures?keywords="+e.join(",")).then(function(n){return"function"==typeof t?t(null,n):void 0},function(){return"function"==typeof t?t({error:"Something went wrong. Flickr unavailable?"},null):void 0})},getInitialPics:function(e){return n.get("/picturesInit").then(function(n){return"function"==typeof e?e(null,n):void 0},function(){return"function"==typeof e?e({error:"Something went wrong. Flickr unavailable?"},null):void 0})}}}]);
+'use strict';
+var App;
+
+App = angular.module('app', ['ngCookies', 'ngResource', 'ngRoute', 'app.controllers', 'app.directives', 'app.filters', 'app.services', 'partials']);
+
+App.config([
+  '$routeProvider', '$locationProvider', function($routeProvider, $locationProvider, config) {
+    $routeProvider.when('/', {
+      templateUrl: '/partials/landingpage.html'
+    }).when('/pictures', {
+      templateUrl: '/partials/pictures.html'
+    }).otherwise({
+      redirectTo: '/'
+    });
+    return $locationProvider.html5Mode(false);
+  }
+]);
+;'use strict';
+/* Controllers*/
+
+angular.module('app.controllers', []).controller('AppCtrl', [
+  '$scope', '$location', '$resource', '$rootScope', function($scope, $location, $resource, $rootScope) {
+    $scope.$location = $location;
+    $scope.$watch('$location.path()', function(path) {
+      return $scope.activeNavId = path || '/';
+    });
+    return $scope.getClass = function(id) {
+      if ($scope.activeNavId.substring(0, id.length) === id) {
+        return 'active';
+      } else {
+        return '';
+      }
+    };
+  }
+]).controller('PicturesCtrl', [
+  '$scope', 'Pictures', function($scope, Pictures) {
+    $scope.currentPictures = [
+      {
+        tags: ["Sonne", "Strand", "Meer"],
+        title: "tramonto in Grecia (Loutraki - golfo di Corinto)",
+        url: "http://farm8.staticflickr.com/7295/13972537026_913a8a116b.jpg"
+      }, {
+        title: "Lac de Capitello, een paternostermeer, Corsica Frankrijk 2002",
+        url: "http://farm6.staticflickr.com/5218/13981158706_c497a0feff.jpg",
+        tags: ["lacdecapitello", "paternostermeer", "meer", "lake", "lac", "corsica", "corse", "frankrijk", "france", "2002"]
+      }, {
+        "url": "http://farm3.staticflickr.com/2914/14355218095_6e23e19d27.jpg",
+        "title": "Northern values",
+        "tags": ["ricohgr", "urbanfragments", "vsco", "chipshop", "cafe"]
+      }
+    ];
+    $scope.requestPictures = function(keywords) {
+      return Pictures.getForKeywords(keywords, function(error, result) {
+        if (error) {
+          return alert(error);
+        } else {
+          console.log(result.data);
+          return $scope.currentPictures = result.data;
+        }
+      });
+    };
+    $scope.nextPicturesByUrl = function(sourceUrl) {
+      var pic;
+      pic = _.filter($scope.currentPictures, function(p) {
+        return p.url === sourceUrl;
+      })[0];
+      return $scope.requestPictures((pic != null ? pic.tags : void 0) || []);
+    };
+    return $scope.nextPicturesByTag = function(tag) {
+      return $scope.requestPictures([tag]);
+    };
+  }
+]);
+;'use strict';
+/* Directives*/
+
+angular.module('app.directives', ['app.services']).directive('appVersion', [
+  'version', function(version) {
+    return function(scope, elm, attrs) {
+      return elm.text(version);
+    };
+  }
+]);
+;'use strict';
+/* Filters*/
+
+angular.module('app.filters', []).filter('interpolate', [
+  'version', function(version) {
+    return function(text) {
+      return String(text).replace(/\%VERSION\%/mg, version);
+    };
+  }
+]);
+;'use strict';
+/* Sevices*/
+
+angular.module('app.services', []).service("User", [
+  "$http", function($http) {
+    var user;
+    user = {
+      name: null,
+      loggedIn: false
+    };
+    return {
+      currentUser: function() {
+        return user;
+      },
+      login: function(id, fn) {
+        return $http.post("/login", {
+          id: id
+        }).then(function(data) {
+          return typeof fn === "function" ? fn(null, data) : void 0;
+        }, function(data) {
+          return typeof fn === "function" ? fn({
+            error: data.error || "Node ID invalid!"
+          }, null) : void 0;
+        });
+      },
+      logout: function() {
+        return $http.post("/logout").then(function(result) {
+          return user = {
+            name: null,
+            loggedIn: false
+          };
+        }, function(result) {
+          return user = {
+            name: null,
+            loggedIn: false
+          };
+        });
+      },
+      checkSession: function() {
+        return $http.get("/loggedin").then(function(result) {
+          user.name = result.data.name;
+          return user.loggedIn = true;
+        }, function(fail) {});
+      }
+    };
+  }
+]).service("Pictures", [
+  "$http", function($http) {
+    return {
+      getForKeywords: function(ary, fn) {
+        return $http.get("/pictures?keywords=" + (ary.join(','))).then(function(data) {
+          return typeof fn === "function" ? fn(null, data) : void 0;
+        }, function(data) {
+          return typeof fn === "function" ? fn({
+            error: "Something went wrong. Flickr unavailable?"
+          }, null) : void 0;
+        });
+      },
+      getInitialPics: function(fn) {
+        return $http.get("/picturesInit").then(function(data) {
+          return typeof fn === "function" ? fn(null, data) : void 0;
+        }, function(data) {
+          return typeof fn === "function" ? fn({
+            error: "Something went wrong. Flickr unavailable?"
+          }, null) : void 0;
+        });
+      }
+    };
+  }
+]);
+;
+//# sourceMappingURL=app.js.map

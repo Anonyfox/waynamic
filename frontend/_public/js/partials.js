de9862584ea428182641647195c68e3717077d7b
jkoschwitz <josua@informatik.tu-chemnitz.de>
2014-09-09 15:19:32 +0200

    show demand to choose a user in profile, if no user has been choosen

--- frontend/_public/js/partials.js
+++ frontend/_public/js/partials.js
@@ -1,22 +1,5 @@
 angular.module('partials', [])
 .run(['$templateCache', function($templateCache) {
-  return $templateCache.put('/partials/landingpage.html', [
-'',
-'<h1>awesome landingpage here.</h1>',''].join("\n"));
-}])
-.run(['$templateCache', function($templateCache) {
-  return $templateCache.put('/partials/nav.html', [
-'',
-'<ul class="nav">',
-'  <li ng-class="getClass(\'/pictures\')"><a ng-href="#/pictures"><i class="fa fa-picture-o"></i> Pictures</a></li>',
-'  <li ng-class="getClass(\'/profile\')"><a ng-href="#/profile"><i class="fa fa-user"></i> Profile</a></li>',
-'</ul>',
-'<form class="navbar-form pull-right">',
-'  <select ng-model="users.current" ng-options="u.name for u in users.list" ng-change="changeSelectedUser(users.current)">',
-'  </select>',
-'</form>',''].join("\n"));
-}])
-.run(['$templateCache', function($templateCache) {
   return $templateCache.put('/partials/pictures.html', [
 '',
 '<div ng-if="users.current._id == -1">',
@@ -39,23 +22,45 @@ angular.module('partials', [])
 '</div>',''].join("\n"));
 }])
 .run(['$templateCache', function($templateCache) {
+  return $templateCache.put('/partials/landingpage.html', [
+'',
+'<h1>awesome landingpage here.</h1>',''].join("\n"));
+}])
+.run(['$templateCache', function($templateCache) {
   return $templateCache.put('/partials/profile.html', [
 '',
-'<div ng-controller="ProfileCtrl">',
-'  <h1 class="text-center">{{users.current.name}}</h1>',
-'  <div style="background: rgba(255,255,255,0.7); margin-bottom: 20px;" class="text-center">',
-'    <h3>Friends: {{users.currentFriends.length}}</h3>',
-'    <button ng-click="switchUserTo(f._id)" ng-repeat="f in users.currentFriends" style="margin: 5px;" class="btn btn-link"><i class="fa fa-user"></i> {{f.firstName}} {{f.lastName}}</button>',
-'  </div>',
-'  <div class="row">',
-'    <div ng-repeat="m in users.currentHistory" class="span4">',
-'      <div style="background: rgba(255,255,255,0.5); margin-bottom: 20px;" class="media"><a class="pull-left"><img ng-src="{{m.url}}" style="height: 75px; width: 75px;" class="media-object"></a>',
-'        <div class="media-body">',
-'          <h4 class="media-heading">{{m.title | shortText}}</h4>',
-'          <p>clicked on {{m.updated | date : \'shortDate\'}}</p>',
+'<div ng-if="users.current._id == -1">',
+'  <h3>Please choose a user !</h3>',
+'</div>',
+'<div ng-if="users.current._id != -1">',
+'  <div ng-controller="ProfileCtrl">',
+'    <h1 class="text-center">{{users.current.name}}</h1>',
+'    <div style="background: rgba(255,255,255,0.7); margin-bottom: 20px;" class="text-center">',
+'      <h3>Friends: {{users.currentFriends.length}}</h3>',
+'      <button ng-click="switchUserTo(f._id)" ng-repeat="f in users.currentFriends" style="margin: 5px;" class="btn btn-link"><i class="fa fa-user"></i> {{f.firstName}} {{f.lastName}}</button>',
+'    </div>',
+'    <div class="row">',
+'      <div ng-repeat="m in users.currentHistory" class="span4">',
+'        <div style="background: rgba(255,255,255,0.5); margin-bottom: 20px;" class="media"><a class="pull-left"><img ng-src="{{m.url}}" style="height: 75px; width: 75px;" class="media-object"></a>',
+'          <div class="media-body">',
+'            <h4 class="media-heading">{{m.title | shortText}}</h4>',
+'            <p>clicked on {{m.updated | date : \'shortDate\'}}</p>',
+'          </div>',
 '        </div>',
 '      </div>',
 '    </div>',
 '  </div>',
 '</div>',''].join("\n"));
+}])
+.run(['$templateCache', function($templateCache) {
+  return $templateCache.put('/partials/nav.html', [
+'',
+'<ul class="nav">',
+'  <li ng-class="getClass(\'/pictures\')"><a ng-href="#/pictures"><i class="fa fa-picture-o"></i> Pictures</a></li>',
+'  <li ng-class="getClass(\'/profile\')"><a ng-href="#/profile"><i class="fa fa-user"></i> Profile</a></li>',
+'</ul>',
+'<form class="navbar-form pull-right">',
+'  <select ng-model="users.current" ng-options="u.name for u in users.list" ng-change="changeSelectedUser(users.current)">',
+'  </select>',
+'</form>',''].join("\n"));
 }]);
